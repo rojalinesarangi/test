03e6a2fdc64dc90394bc6e667e68e43b5850cbe1
rojalinesarangi <102286858+rojalinesarangi@users.noreply.github.com>
2024-01-04 12:14:49 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:-values can be integer or string and expressions will be mathematical operators.                                                                                                           *                                                                                                                              'hello'                                                                                                                    -87.8                                                                                                                             -                                                                                                                               /                                                                                                                               +                                                                                                                               6
+# 
+# -->
+# Values are 'hello' , -87.8 ,6 and expressions are *,/,+,-
+# 
+# 2. What is the difference between string and variable?
+# 
+# -->
+# A Variable is a store of information and a String is a type of information you would store in a Variable.
+# 
+# 3. Describe three different data types.
+# 
+# -->
+# Integer-These are zero,positive and negative whole numbers of unlimited length without a fractional part.It is denoted by int.
+# 
+# List-Lists are used to store multiple items in a single variable and lists are those datatypes which are mutable.Lists are created using square brackets.
+# 
+# Tuple-Tuples are type of data structure that very similar to lists but these are immutable.Tuples are created using paranthesis brackets.
+# 
+# 
+# 
+# 4. What is an expression made up of? What do all expressions do?
+# -->
+# An expression is a combination of values,variables and operators that evaluates to a single value.Expressions are representations of value.For example any string is also an expression since it represents the value of the string as well. 
+# 
+# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# -->
+# A statement in python is used for creating variables or for displaying values.The expression in python produces some value or result after being interpreted by the python interpreter.A statement in python is not evaluated for some results.An expression in python is evaluated for some results.
+# 
+# 6. After running the following code, what does the variable bacon contain?                                                   bacon = 22                                                                                                                      bacon + 1
+# -->
+# 23
+# 
+# 7. What should the values of the following two terms be?                                                                        'spam' + 'spamspam'                                                                                                              'spam' * 3
+# -->
+# 'spam'+'spamspam'='spamspamspam'
+# 'spam'* 3 =' spamspamspam'
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# -->
+# Eggs is a variable name while 100 is invalid because variable names cannot begin with a number.
+# 
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+# -->
+# Function used for integer is int().
+# Function used for floating point number is float().
+# Function used for string is str().
+# 
+# 10. Why does this expression cause an error? How can you fix it?                                                                 'I have eaten ' + 99 + ' burritos.'
+# -->
+# This expression cause an error because a string to string concatenation is only possible but 99 is an integer so it is showing error.It could be fixed by this expression  ' I have eaten ' + ' 99 ' + ' burritos.'
